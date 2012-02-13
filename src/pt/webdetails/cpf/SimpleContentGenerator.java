d0a4039f26969eaff25ed5a703d3f92bde047862
tgf <tiago.ferreira@webdetails.pt>
2012-02-13 16:08:44 +0000

    changed dist target zips for CI; prevent NPE in SimpleContentGenerator

--- src/pt/webdetails/cpf/SimpleContentGenerator.java
+++ src/pt/webdetails/cpf/SimpleContentGenerator.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.pentaho.platform.api.engine.IParameterProvider;
@@ -36,28 +38,36 @@ public class SimpleContentGenerator extends BaseContentGenerator {
             final OutputStream out = contentItem.getOutputStream(null);
             final Class<?>[] params = {OutputStream.class};
 
-            final String method = pathParams.getStringParameter("path", null).split("/")[1].toLowerCase();
-
-            try {
-                final Method mthd = this.getClass().getMethod(method, params);
-                boolean exposed = mthd.isAnnotationPresent(Exposed.class);
-                boolean accessible = exposed && mthd.getAnnotation(Exposed.class).accessLevel() == AccessLevel.PUBLIC;
-                if (accessible) {
-                    mthd.invoke(this, out);
-                } else {
-                    throw new IllegalAccessException("Method " + method + " has the wrong access level");
-                }
-            } catch (NoSuchMethodException e) {
-                logger.warn("could't locate method: " + method);
-            } catch (InvocationTargetException e) {
-                logger.error(e.toString());
-
-            } catch (IllegalAccessException e) {
-                logger.warn(e.toString());
-
-            } catch (IllegalArgumentException e) {
-
-                logger.error(e.toString());
+            String[] pathSections = StringUtils.split(pathParams.getStringParameter("path", null),"/");
+            
+            if(pathSections != null && pathSections.length > 1){
+              
+              final String method = StringUtils.lowerCase(pathSections[1]);
+  
+              try {
+                  final Method mthd = this.getClass().getMethod(method, params);
+                  boolean exposed = mthd.isAnnotationPresent(Exposed.class);
+                  boolean accessible = exposed && mthd.getAnnotation(Exposed.class).accessLevel() == AccessLevel.PUBLIC;
+                  if (accessible) {
+                      mthd.invoke(this, out);
+                  } else {
+                      throw new IllegalAccessException("Method " + method + " has the wrong access level");
+                  }
+              } catch (NoSuchMethodException e) {
+                  logger.warn("could't locate method: " + method);
+              } catch (InvocationTargetException e) {
+                  logger.error(e.toString());
+  
+              } catch (IllegalAccessException e) {
+                  logger.warn(e.toString());
+  
+              } catch (IllegalArgumentException e) {
+  
+                  logger.error(e.toString());
+              }
+            }
+            else {
+              logger.error("No method supplied.");
             }
         } catch (SecurityException e) {
             logger.warn(e.toString());
