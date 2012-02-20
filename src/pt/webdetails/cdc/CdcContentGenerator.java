17746fad4abe9f4207236a6570c450e4df7e9f66
Pedro Pinheiro <pdpinheiro@gmail.com>
2012-02-20 10:03:36 +0000

    Update CacheClean dashboard

--- src/pt/webdetails/cdc/CdcContentGenerator.java
+++ src/pt/webdetails/cdc/CdcContentGenerator.java
@@ -1,17 +1,19 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 package pt.webdetails.cdc;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
+import javax.servlet.ServletRequestWrapper;
+import org.pentaho.platform.api.engine.IParameterProvider;
 import pt.webdetails.cpf.InterPluginComms;
 import pt.webdetails.cpf.SimpleContentGenerator;
 import pt.webdetails.cpf.annotations.AccessLevel;
 import pt.webdetails.cpf.annotations.Exposed;
+import pt.webdetails.cpf.olap.OlapUtils;
 
 /**
  *
@@ -19,51 +21,52 @@ import pt.webdetails.cpf.annotations.Exposed;
  */
 public class CdcContentGenerator extends SimpleContentGenerator {
 
-  private static final long serialVersionUID = 1L;
-  
-  public static final String ENCODING = "utf-8";
-  
-  private static final String CDE_PLUGIN = "pentaho-cdf-dd";
-  
+    private static final long serialVersionUID = 1L;
+    public static final String ENCODING = "utf-8";
+    private static final String CDE_PLUGIN = "pentaho-cdf-dd";
+
     @Exposed(accessLevel = AccessLevel.PUBLIC)
     public void edit(OutputStream out) throws IOException {
+        IParameterProvider pathParams = parameterProviders.get("path");
+        ServletRequestWrapper wrapper = (ServletRequestWrapper) pathParams.getParameter("httprequest");
+        String root = wrapper.getScheme() + "://" + wrapper.getServerName() + ":" + wrapper.getServerPort();
         Map<String, Object> params = new HashMap<String, Object>();
         params.put("solution", "system");
         params.put("path", "cdc/presentation/");
         params.put("file", "cdcHome.wcdf");
         params.put("absolute", "true");
-        params.put("root", "http://localhost:8080");
+        params.put("root", getRoot());
         out.write(InterPluginComms.callPlugin(CDE_PLUGIN, "Render", params).getBytes(ENCODING));
     }
 
     @Exposed(accessLevel = AccessLevel.PUBLIC)
     public void home(OutputStream out) throws IOException {
-    	Map<String, Object> params = getLink("cdcHome.wcdf");
-    	run(out, params);
+        Map<String, Object> params = getLink("cdcHome.wcdf");
+        run(out, params);
     }
 
     @Exposed(accessLevel = AccessLevel.PUBLIC)
     public void clusterinfo(OutputStream out) throws IOException {
-    	Map<String, Object> params = getLink("cdcClusterInfo.wcdf");
-    	run(out, params);
+        Map<String, Object> params = getLink("cdcClusterInfo.wcdf");
+        run(out, params);
     }
 
     @Exposed(accessLevel = AccessLevel.PUBLIC)
     public void cacheinfo(OutputStream out) throws IOException {
-    	Map<String, Object> params = getLink("cdcCacheInfo.wcdf");
-    	run(out, params);
+        Map<String, Object> params = getLink("cdcCacheInfo.wcdf");
+        run(out, params);
     }
 
     @Exposed(accessLevel = AccessLevel.PUBLIC)
     public void settings(OutputStream out) throws IOException {
-    	Map<String, Object> params = getLink("cdcSettings.wcdf");
-    	run(out, params);
+        Map<String, Object> params = getLink("cdcSettings.wcdf");
+        run(out, params);
     }
 
     @Exposed(accessLevel = AccessLevel.PUBLIC)
     public void cacheclean(OutputStream out) throws IOException {
-      	Map<String, Object> params = getLink("cdcCacheClean.wcdf");
-      	run(out, params);
+        Map<String, Object> params = getLink("cdcCacheClean.wcdf");
+        run(out, params);
     }
 
     private Map<String, Object> getLink(String dashboardName) {
@@ -73,17 +76,32 @@ public class CdcContentGenerator extends SimpleContentGenerator {
         params.put("file", dashboardName);
         params.put("bypassCache", "true");
         params.put("absolute", "true");
-        params.put("root", "localhost:8080");
-	
-	return params;
-    }
+        params.put("debug", "true");
+        params.put("root", getRoot());
 
+        return params;
+    }
 
     private void run(OutputStream out, Map<String, Object> params) throws IOException {
         out.write(InterPluginComms.callPlugin(CDE_PLUGIN, "Render", params).getBytes(ENCODING));
     }
 
+    private String getRoot() {
 
+        IParameterProvider pathParams = parameterProviders.get("path");
+        ServletRequestWrapper wrapper = (ServletRequestWrapper) pathParams.getParameter("httprequest");
+        String root = wrapper.getServerName() + ":" + wrapper.getServerPort();
+        return root;
+    }
 
-
+    @Exposed(accessLevel = AccessLevel.PUBLIC)
+    public void olaputils(OutputStream out) {
+        OlapUtils utils = new OlapUtils();
+        IParameterProvider requestParams = parameterProviders.get("request");
+        try {
+            out.write(utils.process(requestParams).toString().getBytes("utf-8"));
+        } catch (IOException e) {
+            logger.error(e);
+        }
+    }
 }
--- src/pt/webdetails/cpf/SimpleContentGenerator.java
+++ src/pt/webdetails/cpf/SimpleContentGenerator.java
@@ -1,7 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 package pt.webdetails.cpf;
 
 import java.io.IOException;
@@ -24,50 +23,48 @@ import pt.webdetails.cpf.annotations.Exposed;
  */
 public class SimpleContentGenerator extends BaseContentGenerator {
 
-
-  private static final long serialVersionUID = 1L;
-    Log logger = LogFactory.getLog(this.getClass());
+    private static final long serialVersionUID = 1L;
+    protected Log logger = LogFactory.getLog(this.getClass());
 
     @Override
     public void createContent() {
         IParameterProvider pathParams = parameterProviders.get("path");
-                //requestParams = parameterProviders.get("request");
+        //requestParams = parameterProviders.get("request");
         final IContentItem contentItem = outputHandler.getOutputContentItem("response", "content", "", instanceId, "text/html");
 
         try {
             final OutputStream out = contentItem.getOutputStream(null);
             final Class<?>[] params = {OutputStream.class};
 
-            String[] pathSections = StringUtils.split(pathParams.getStringParameter("path", null),"/");
-            
-            if(pathSections != null && pathSections.length > 0){
-              
-              final String method = StringUtils.lowerCase(pathSections[0]);
-  
-              try {
-                  final Method mthd = this.getClass().getMethod(method, params);
-                  boolean exposed = mthd.isAnnotationPresent(Exposed.class);
-                  boolean accessible = exposed && mthd.getAnnotation(Exposed.class).accessLevel() == AccessLevel.PUBLIC;
-                  if (accessible) {
-                      mthd.invoke(this, out);
-                  } else {
-                      throw new IllegalAccessException("Method " + method + " has the wrong access level");
-                  }
-              } catch (NoSuchMethodException e) {
-                  logger.warn("could't locate method: " + method);
-              } catch (InvocationTargetException e) {
-                  logger.error(e.toString());
-  
-              } catch (IllegalAccessException e) {
-                  logger.warn(e.toString());
-  
-              } catch (IllegalArgumentException e) {
-  
-                  logger.error(e.toString());
-              }
-            }
-            else {
-              logger.error("No method supplied.");
+            String[] pathSections = StringUtils.split(pathParams.getStringParameter("path", null), "/");
+
+            if (pathSections != null && pathSections.length > 0) {
+
+                final String method = StringUtils.lowerCase(pathSections[0]);
+
+                try {
+                    final Method mthd = this.getClass().getMethod(method, params);
+                    boolean exposed = mthd.isAnnotationPresent(Exposed.class);
+                    boolean accessible = exposed && mthd.getAnnotation(Exposed.class).accessLevel() == AccessLevel.PUBLIC;
+                    if (accessible) {
+                        mthd.invoke(this, out);
+                    } else {
+                        throw new IllegalAccessException("Method " + method + " has the wrong access level");
+                    }
+                } catch (NoSuchMethodException e) {
+                    logger.warn("could't locate method: " + method);
+                } catch (InvocationTargetException e) {
+                    logger.error(e.toString());
+
+                } catch (IllegalAccessException e) {
+                    logger.warn(e.toString());
+
+                } catch (IllegalArgumentException e) {
+
+                    logger.error(e.toString());
+                }
+            } else {
+                logger.error("No method supplied.");
             }
         } catch (SecurityException e) {
             logger.warn(e.toString());
