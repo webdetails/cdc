e1e5d2a99f91ddb1607f1058f0819fc77bf5bf30
Pedro Vale <pedro.vale@webdetails.pt>
2012-03-27 19:01:56 +0100

    Added admin validation. Added bat and sh files instead of build.xml generation

---
+++ hazelcast-standalone/launch-hazelcast.bat
@@ -0,0 +1,3 @@
+@echo off
+java -Xmx1024m -Djava.net.preferIPv4Stack=true -Dsun.lang.ClassLoader.allowArraySyntax=true -Dhazelcast.serializer.shared=true  %JAVA_OPTS% -cp lib\hazelcast-*.jar;lib\* com.hazelcast.examples.StartServer
+        
--- src/pt/webdetails/cdc/CdcContentGenerator.java
+++ src/pt/webdetails/cdc/CdcContentGenerator.java
@@ -9,7 +9,15 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import javax.servlet.ServletRequestWrapper;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.pentaho.platform.api.engine.IPentahoSession;
 import org.pentaho.platform.api.engine.IParameterProvider;
+import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
+import org.pentaho.platform.engine.security.SecurityHelper;
+
+
 import pt.webdetails.cpf.InterPluginComms;
 import pt.webdetails.cpf.SimpleContentGenerator;
 import pt.webdetails.cpf.annotations.AccessLevel;
@@ -25,50 +33,65 @@ public class CdcContentGenerator extends SimpleContentGenerator {
     private static final long serialVersionUID = 1L;
     public static final String ENCODING = "utf-8";
 
-    @Exposed(accessLevel = AccessLevel.PUBLIC)
-    public void edit(OutputStream out) throws IOException {
-        IParameterProvider pathParams = parameterProviders.get("path");
-        ServletRequestWrapper wrapper = (ServletRequestWrapper) pathParams.getParameter("httprequest");
-        String root = wrapper.getScheme() + "://" + wrapper.getServerName() + ":" + wrapper.getServerPort();
-        Map<String, Object> params = new HashMap<String, Object>();
-        params.put("solution", "system");
-        params.put("path", "cdc/presentation/");
-        params.put("file", "cdcHome.wcdf");
-        params.put("absolute", "true");
-        params.put("root", getRoot());
-        out.write(InterPluginComms.callPlugin(InterPluginComms.Plugin.CDE, "Render", params).getBytes(ENCODING));
-    }
-
+  
     @Exposed(accessLevel = AccessLevel.PUBLIC)
     public void home(OutputStream out) throws IOException {
+        if (!validateAccess())
+            return;
+        
         Map<String, Object> params = getLink("cdcHome.wcdf");
         run(out, params);
     }
 
     @Exposed(accessLevel = AccessLevel.PUBLIC)
     public void clusterinfo(OutputStream out) throws IOException {
+        if (!validateAccess())
+            return;
+        
         Map<String, Object> params = getLink("cdcClusterInfo.wcdf");
         run(out, params);
     }
 
     @Exposed(accessLevel = AccessLevel.PUBLIC)
-    public void cacheinfo(OutputStream out) throws IOException {
+    public void cacheinfo(OutputStream out) throws IOException {        
+        if (!validateAccess())
+            return;
+                
         Map<String, Object> params = getLink("cdcCacheInfo.wcdf");
         run(out, params);
     }
 
     @Exposed(accessLevel = AccessLevel.PUBLIC)
     public void settings(OutputStream out) throws IOException {
+        if (!validateAccess())
+            return;
+                
         Map<String, Object> params = getLink("cdcSettings.wcdf");
         run(out, params);
     }
 
     @Exposed(accessLevel = AccessLevel.PUBLIC) 
     public void cacheclean(OutputStream out) throws IOException {
+        if (!validateAccess())
+            return;
+                        
         Map<String, Object> params = getLink("cdcCacheClean.wcdf");
         run(out, params);
     }
 
+    
+    private boolean validateAccess() {
+        IPentahoSession userSession = PentahoSessionHolder.getSession();
+        if (!SecurityHelper.isPentahoAdministrator(userSession)) {
+            final HttpServletResponse response = (HttpServletResponse) parameterProviders.get("path").getParameter("httpresponse");
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            logger.warn("Access denied for user: " + userSession.getName() + ". Not an admin");
+            return false;
+        }
+        
+        return true;                
+    }
+    
     private Map<String, Object> getLink(String dashboardName) {
         Map<String, Object> params = new HashMap<String, Object>();
         params.put("solution", "system");
