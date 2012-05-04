31fbe1d873213b2026c5762ec3fde0d012c6a234
Pedro Alves <pedro.alves@webdetails.pt>
2012-05-04 10:56:12 +0100

    - Fixed sh launcher for standalone app

--- hazelcast-standalone/launch-hazelcast.sh
+++ hazelcast-standalone/launch-hazelcast.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-java -Xmx1024m -Djava.net.preferIPv4Stack=true -Dsun.lang.ClassLoader.allowArraySyntax=true -Dhazelcast.serializer.shared=true  %JAVA_OPTS% -cp lib\hazelcast-*.jar;lib\* com.hazelcast.examples.StartServer
-        
+java -Xmx1024m -Djava.net.preferIPv4Stack=true -Dsun.lang.ClassLoader.allowArraySyntax=true -Dhazelcast.serializer.shared=true  $JAVA_OPTS -cp lib/hazelcast-*.jar;lib/* com.hazelcast.examples.StartServer
