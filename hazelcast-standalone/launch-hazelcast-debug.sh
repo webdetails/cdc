59ff3e5dbaa9d1c17639cefd01e3b678cc68d326
Pedro Alves <pedro.alves@webdetails.pt>
2012-05-21 18:06:53 +0100

    - Changed standalone scripts to change to basedir before executing

--- hazelcast-standalone/launch-hazelcast-debug.sh
+++ hazelcast-standalone/launch-hazelcast-debug.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+BASEDIR=`dirname $0`
+cd $BASEDIR
+
 java -Xmx1024m -Djava.net.preferIPv4Stack=true -Dsun.lang.ClassLoader.allowArraySyntax=true -Dhazelcast.serializer.shared=true  $JAVA_OPTS -cp lib/hazelcast-*.jar:lib/* pt.webdetails.cdc.hazelcast.CfgTestApp cdaCache
         
+cd -
--- hazelcast-standalone/launch-hazelcast.sh
+++ hazelcast-standalone/launch-hazelcast.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+
+BASEDIR=`dirname $0`
+cd $BASEDIR
+
 while java -Xmx1024m -Xms256m -Djava.net.preferIPv4Stack=true -Dsun.lang.ClassLoader.allowArraySyntax=true -Dhazelcast.serializer.shared=true  $JAVA_OPTS -cp lib/hazelcast-*.jar:lib/* pt.webdetails.cdc.hazelcast.StartServer
 do
 echo "## Restarting... ##"
 done
+
+cd -
