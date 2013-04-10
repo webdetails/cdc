1f34348665f033d03a7b7909f5633ffdee1d1519
tgf <tiago.n.ferreira@gmail.com>
2013-04-10 17:25:47 +0100

    StartServer: remove deprecated

--- src/pt/webdetails/cdc/hazelcast/StartServer.java
+++ src/pt/webdetails/cdc/hazelcast/StartServer.java
@@ -5,53 +5,58 @@
 package pt.webdetails.cdc.hazelcast;
 
 import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 
 public class StartServer extends com.hazelcast.examples.StartServer {
-  
-  
+
+  public static int ERR_INI = 1;
+  public static int ERR_ILLEGAL_STATE = 2;
+  public static int ERR_OTHER = 3;
+
   public static void main(String[] args) 
   {
-      try{
-        Hazelcast.getConfig().setCheckCompatibility(false);
-        Hazelcast.getCluster();
-      }
-      catch(Exception e){
-        System.out.println("LAUNCH EXIT:" + e);
-        Runtime.getRuntime().exit(3);
-      }
-    Hazelcast.addInstanceListener(new ExitingInstanceListener(3));
-    new Thread(null, new Runnable(){
-
-      @Override
-      public void run() {
-        boolean exit = false;
-        while(!exit){
-          try {
-            Thread.sleep(5000);
-            try{
-              if(!Hazelcast.getLifecycleService().isRunning()){
-                System.exit(5);
-              }
-//              Hazelcast.getMap("cdaCache");
-            }
-            catch(IllegalStateException e){
-              System.out.println("ILLEGAL STATE EXCEPTION EXIT");
-              System.exit(5);
-            }
-            catch(Exception e){
-              System.out.println("EXCEPTION EXIT");
-              System.exit(7);
-            }
-          } catch (InterruptedException e) {
-            System.out.println("Interrupted!");
-            System.exit(0);
-          }
-        }
-      }
+    try {
+      HazelcastInstance hazelcast = Hazelcast.newHazelcastInstance();
+      hazelcast.getConfig().setCheckCompatibility(false);
+      hazelcast.getCluster();
+      hazelcast.addInstanceListener(new ExitingInstanceListener(3));
       
-    }, "check hazelcast death" ).start();
+      new Thread(null, new PulseChecker(hazelcast), "check hazelcast death" ).start();
+    }
+    catch(Exception e){
+      System.out.println("LAUNCH EXIT:" + e);
+      Runtime.getRuntime().exit(ERR_INI);
+    }
   }
 
-  
+  static class PulseChecker implements Runnable {
+    HazelcastInstance hazelcast;
+    public PulseChecker(HazelcastInstance hz) {
+      this.hazelcast = hz;
+    }
+    public void run() {
+      boolean exit = false;
+      while(!exit){
+        try {
+          Thread.sleep(5000);
+          try{
+            if(!hazelcast.getLifecycleService().isRunning()){
+              System.exit(5);
+            }
+          }
+          catch(IllegalStateException e){
+            System.out.println(e.getLocalizedMessage());
+            System.exit(ERR_ILLEGAL_STATE);
+          }
+          catch(Exception e){
+            e.printStackTrace();
+            System.exit(ERR_OTHER);
+          }
+        } catch (InterruptedException e) {
+          System.exit(0);
+        }
+      }
+    }
+  }
   
 }
