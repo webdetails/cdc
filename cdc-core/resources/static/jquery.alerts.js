5991f8626de47a135afdd844d63d59c23cb8705f
Pedro Vale <pedro.vale@webdetails.pt>
2016-09-13 21:51:11 +0100

    Fix for jquery new version

--- cdc-core/resources/static/jquery.alerts.js
+++ cdc-core/resources/static/jquery.alerts.js
@@ -85,7 +85,7 @@
 			if( $.alerts.dialogClass ) $("#popup_container").addClass($.alerts.dialogClass);
 			
 			// IE6 Fix
-			var pos = ($.browser.msie && parseInt($.browser.version) <= 6 ) ? 'absolute' : 'fixed'; 
+			var pos = 'fixed'; 
 			
 			$("#popup_container").css({
 				position: pos,
@@ -197,8 +197,6 @@
 			if( top < 0 ) top = 0;
 			if( left < 0 ) left = 0;
 			
-			// IE6 fix
-			if( $.browser.msie && parseInt($.browser.version) <= 6 ) top = top + $(window).scrollTop();
 			
 			$("#popup_container").css({
 				top: top + 'px',
